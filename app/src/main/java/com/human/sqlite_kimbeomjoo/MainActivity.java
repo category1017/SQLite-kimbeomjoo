1f793a83251b71f3a79e463794aa38a15917d5a1
category1017 <category1017@gmail.com>
2021-03-17 11:42:22 +0900

    버튼 프로그램 구현(수정, 삭제 기능)

--- app/src/main/java/com/human/sqlite_kimbeomjoo/MainActivity.java
+++ app/src/main/java/com/human/sqlite_kimbeomjoo/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,29 +24,117 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase mSqLiteDatabase; //안드로이드에서 만들어준//sql템플릿(insert,select...)이 여기 포함
     private RecyclerAdapter mRecyclerAdapter;
     private List mItemList = new ArrayList<StudentVO>();//객체생성,셀렉트 쿼리결과 저장객체
+    //입력,수정,삭제 EditText 변수 선언 (->객체로 만들어야 사용가능, 객체 생성 항상실행되는 onCreate메서드에 있음)
+    private EditText mEditTextGrade;
+    private EditText mEditTextNumber;
+    private EditText mEditTextName;
+    //현재 RecyclerAdapter에서 선택한 값 확인 변수
+    private int currentCursorId = -1;
+    //버튼 Button 변수선언
+    private Button mButtonInsert;
+    private Button mButtonUpdate;
+    private Button mButtonDelete;
+
+    //메인액티비티가 실행되면, 자동으로 실행되는 메서드 onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);//부모 클래스 초기화(메모리로딩)
+        setContentView(R.layout.activity_main);//화면 렌더링 됨.
         //신규 데이터베이스 객체 생성=메모리에 올리기=실행가능하게 만들기(아래)
         //=데이터베이스헬퍼클래스의 생성자 메서드 실행
         mDatabaseHelper = new DatabaseHelper(this,"student.db",null,1);
         //데이터베이스 파일 만들기(아래)
-        mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();
+        mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();//싱글톤으로 만들어진 메서드
+        // ->싱글톤으로 만들었다는 의미는 인스턴스는 1번만 실행됨.즉 student.db파일이 있으면 생성안됨.
         //테스트로 mSqliteDatabse 객체를 이용해서 더미데이터 인서트 테스트
         //자바의 HashMap형식과 비슷한 안드로이드 데이터형 ContentValues형
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(StudentTable.GRADE,1);
-        contentValues.put(StudentTable.NUMBER,20210001);
-        contentValues.put(StudentTable.NAME,"아무개1");
-        mSqLiteDatabase.insert(StudentTable.TABLE_NAME,null,contentValues);
-
+        /*ContentValues contentValues = new ContentValues();
+        for(int i=1;i<=10;i++){
+            contentValues.put(StudentTable.GRADE,i);
+            contentValues.put(StudentTable.NUMBER,"2021000"+i);
+            contentValues.put(StudentTable.NAME,"아무개"+i);
+            mSqLiteDatabase.insert(StudentTable.TABLE_NAME,null,contentValues);
+        }*/
         //mItemList에 셀렉트 쿼리 결과값이 set되어있어야함.
-
+        //멤버변수로 객체 초기화하기(아래)
+        bindObject();
         //List 실행 리사이클러 어댑터 바인딩(아래)
         bindList();//여기서는 공간마련
-        //List반영(화면출력)
+        //List반영(화면출력 : 입력, 수정, 삭제시 화면 리프레시가 필요하고, 구현하는 메서드가 아래)
         updateList();//여기에서 데이터가 바인딩되어서 RecyclerAdapter가 화면에 재생됩니다.
+        //update버튼 클릭이벤트(아래)
+        btnUpdate();
+        //delete버튼 클릭이벤트(아래)
+        btnDelete();
+    }
+    //delete버튼 클릭이벤트(아래)
+    private void btnDelete() {
+        mButtonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentCursorId == -1){
+                    Toast.makeText(getApplicationContext(),"선택된 값이 없습니다.",Toast.LENGTH_SHORT).show();
+                    return;//선택된 값이 없으면 삭제이벤트는 종료하기
+                }
+                //삭제쿼리 호출
+                deleteData(currentCursorId);
+                //EditText객체의 값 비우기
+                mEditTextGrade.setText("");
+                mEditTextNumber.setText("");
+                mEditTextName.setText("");
+                currentCursorId = -1; //현재 테이블커서ID가 지워졌으니 다시 초기화시킴.
+                //리사이클러뷰 화면 리프레시(아래)
+                updateList();
+            }
+        });
+    }
+    //delete버튼에 대한 쿼리
+    private void deleteData(int currentCursorId) {
+        //SQLiteDatabase 템플릿 delete메서드 실행
+        mSqLiteDatabase.delete(StudentTable.TABLE_NAME,StudentTable._ID+"="+currentCursorId,null);
+    }
+
+    //update버튼 클릭이벤트(아래)
+    private void btnUpdate() {
+        mButtonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentCursorId == -1){
+                    Toast.makeText(getApplicationContext(),"선택된 값이 없습니다.",Toast.LENGTH_SHORT).show();
+                    return;//클릭이벤트 진행중지
+                }
+                //DB갱신
+                final int grade = Integer.parseInt(mEditTextGrade.getText().toString());
+                final int number = Integer.parseInt(mEditTextNumber.getText().toString());
+                final String name = mEditTextName.getText().toString();
+                //쿼리메서드 호출
+                updateData(currentCursorId,grade,number,name);
+                //화면 리프레시 재생(아래)
+                updateList();
+            }
+        });
+    }
+    //update버튼에 대한 쿼리
+    private void updateData(int currentCursorId, int grade, int number, String name) {
+        //쿼리 매개변수로 1개의 객체에 값을 담기 위해서 객체변수 생성(아래)
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(StudentTable.GRADE, grade);
+        contentValues.put(StudentTable.NUMBER, number);
+        contentValues.put(StudentTable.NAME, name);
+        //DB갱신 SQLiteDatabase에 있는 템플릿메서드 중 update사용(아래)
+        mSqLiteDatabase.update(StudentTable.TABLE_NAME,contentValues,StudentTable._ID+"="+currentCursorId,null);
+    }
+
+    //멤버변수로 객체 초기화하기(아래)
+    private void bindObject() {//onCreate 안에 너무 많아서 따로 모아둠
+        //EditText변수를 객체로 생성
+        mEditTextGrade = findViewById(R.id.editTextGrade);
+        mEditTextNumber = findViewById(R.id.editTextNumber);
+        mEditTextName = findViewById(R.id.editTextName);
+        //Button변수를 객체로 생성(아래3줄)
+        mButtonInsert = findViewById(R.id.btnInsert);
+        mButtonUpdate = findViewById(R.id.btnUpdate);
+        mButtonDelete = findViewById(R.id.btnDelete);
     }
 
     private void updateList() {
@@ -79,9 +171,21 @@ public class MainActivity extends AppCompatActivity {
     private void bindList() {
         //객체 생성
         mRecyclerAdapter = new RecyclerAdapter(mItemList);
+        //어댑터의 OnItemClickListener 추가예정...
+        mRecyclerAdapter.setOnItemClickListener(new RecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                StudentVO studentVO = (StudentVO) mItemList.get(position);
+                //디버그
+                currentCursorId = studentVO.getmId();
+                Toast.makeText(getApplicationContext(),"현재 선택한 커서레코드 ID는 "+currentCursorId,Toast.LENGTH_SHORT).show();
+                mEditTextGrade.setText(Integer.toString(studentVO.getmGrade()));
+                mEditTextNumber.setText(Integer.toString(studentVO.getmNumber()));
+                mEditTextName.setText(studentVO.getmName());
+            }
+        });
         //리사이클러뷰xml과 어댑터 바인딩(attach) No adapter attached
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-
         recyclerView.setHasFixedSize(true);//리사이클러 뷰의 높이를 고정한다.
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(mRecyclerAdapter);//실제 attach(바인딩)
